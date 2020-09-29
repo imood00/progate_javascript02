3ad3b79f058d7fda625f7fc37b31edfefc265179
imood <69391851+imood00@users.noreply.github.com>
2020-09-29 12:11:23 +0100

    Add files via upload

---
+++ es6_study_2/page6/script.js
@@ -0,0 +1,9 @@
+const animals = ["dog", "cat", "sheep"];
+
+// 配列の1つ目の要素を出力してください
+console.log(animals);
+
+// 配列の3つ目の要素を出力してください
+console.log(animals[0]);
+console.log(animals[1]);
+console.log(animals[2]);
